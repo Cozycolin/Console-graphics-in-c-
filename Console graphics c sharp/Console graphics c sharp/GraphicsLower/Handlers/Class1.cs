5499f38ba3b00776ff76e2829be7c0fcc4fb6397
Cozycolin <42123514+Cozycolin@users.noreply.github.com>
2021-06-05 23:54:13 -0400

    Add files via upload

---
+++ Console graphics c sharp/Console graphics c sharp/GraphicsLower/Handlers/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphics
+{
+    namespace Handlers
+    {
+        class Frame
+        {
+            public int width;
+            public int height;
+            public char[,] charmap;
+            public byte[,] lightmap;
+            public char clearChar = ' ';
+
+            Frame(int width, int height)
+            {
+                this.width = width;
+                this.height = height;
+                charmap = new char[width,height];
+                lightmap = new byte[width, height];
+            }
+            void resetFrame()
+            {
+                for(int x = 0;x < width;x++)
+                {
+                    for(int y = 0; y < height; y++)
+                    {
+                        charmap[x, y] = clearChar;
+                    }
+                }
+            }
+            
+        }
+    }
+}
---
+++ Console graphics c sharp/Console graphics c sharp/GraphicsLower/Handlers/CoordinatesHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphics
+{
+    namespace Handlers
+    {
+        class Coor
+        {
+            public int x;
+            public int y;
+            Coor(int ix = 0, int iy = 0)
+            {
+                x = ix;
+                y = iy;
+            }
+        }
+    }
+}
