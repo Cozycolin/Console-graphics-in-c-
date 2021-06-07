c7f8265cc7394c44a3e9ded7608959c4f6f119a5
Cozycolin <42123514+Cozycolin@users.noreply.github.com>
2021-06-06 21:39:28 -0400

    Add files via upload

---
+++ Source/GraphicsUpper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Graphics.Handlers;
+
+
+namespace Graphics
+{
+    public class Screen
+    {
+        public readonly int width;
+        public readonly int height;
+        Frame[] swapBuffer;
+        byte currentBuffer = 0;
+        byte currentBufferOpposite = 1;
+        public FrameManipulator frameManipulator;
+        public Handlers.errorHandler.GraphicsErrorHandler errorHandler;
+        public Screen()
+        {
+            frameManipulator = new FrameManipulator();
+            errorHandler = new Handlers.errorHandler.GraphicsErrorHandler();
+            width = Console.WindowWidth;
+            height = Console.WindowHeight;
+            swapBuffer = new Frame[2] { new Frame(this.width, this.height), new Frame(this.width, this.height) };
+            //acknowledgement
+        }
+        public void swapBuffers()
+        {
+            currentBuffer += 0b1;
+            currentBufferOpposite += 0b1;
+            CheckBufferNumbers();
+            frameManipulator.SetActiveFrame(swapBuffer[currentBufferOpposite]);
+        }
+        public void displayFrame()
+        { 
+            Frame frame = swapBuffer[currentBuffer];
+            Console.Clear();
+            if(frame.width != width || frame.height != height)
+            {
+                errorHandler.RaiseError("frame dimensions do not meet graphics dimensions, "
+                    + frame.width +' ' + width + ',' + frame.height + ' ' + height);
+            }
+            for (int x = 0; x < frame.width; x++)
+            {
+                for (int y = 0; y < frame.height; y++)
+                {
+                    if (frame.getLightMap()[x,y] == true)
+                    {
+                        Console.Write(frame.getCharMap()[x, y]);
+                    }
+                }
+            }
+        }
+        void CheckBufferNumbers()
+        {
+            if (currentBuffer > 1)
+            {
+                currentBuffer = 0;
+            }
+            if (currentBufferOpposite > 1)
+            {
+                currentBufferOpposite = 0;
+            }
+        }
+    }
+    
+}
---
+++ Source/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Graphics;
+using Graphics.Handlers.errorHandler;
+namespace Console_graphics_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("program has started");
+            Screen screen = new Screen();
+            screen.swapBuffers();
+            screen.displayFrame();
+            Console.ReadKey();
+        }
+    }
+}
